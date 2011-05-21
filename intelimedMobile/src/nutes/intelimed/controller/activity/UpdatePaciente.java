e54b18315d205e7aa81c3bfd2592b4e7c537b9f0
dyego12345 <dyego12345@gmail.com>
2011-05-21 18:23:28 +0000

    - Criação de radio buttons para opção de editar e deletar no layout paciente.xml - Tratamento da habilitação dos campos e implementação da funcionalidade "Remover Paciente" UpdatePaciente.java - Implementação do método deletar() em PacienteDao.java

--- intelimedMobile/src/nutes/intelimed/controller/activity/UpdatePaciente.java
+++ intelimedMobile/src/nutes/intelimed/controller/activity/UpdatePaciente.java
@@ -7,27 +7,40 @@ import nutes.intelimed.model.entity.Paciente;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class UpdatePaciente extends Activity implements OnClickListener{
 	public static InterfaceModelPaciente dao;
 	EditText nome, dtnascimento;
-	Button upPaciente,delPaciente;
+	Button upPaciente;
 	Paciente paciente;
+	RadioButton delPaciente,edPaciente;
+	RadioGroup radioGroup;
+	boolean del=false;
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		dao = new PacienteScript(this);
 		setContentView(R.layout.paciente);
+		
 		nome = (EditText) findViewById(R.paciente.nome);
         dtnascimento = (EditText) findViewById(R.paciente.dtnascimento);
         upPaciente = (Button) findViewById(R.bt.cadPaciente);
-        //delPaciente= (Button) findViewById(R.bt.delPaciente);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        delPaciente = (RadioButton) findViewById(R.id.remover);
+        edPaciente = (RadioButton) findViewById(R.id.editar);
         upPaciente.setText("Atualizar");
         upPaciente.setOnClickListener(this);
-        
+        radioGroup.setVisibility(RadioGroup.VISIBLE);
+        delPaciente.setVisibility(RadioButton.VISIBLE);
+        edPaciente.setVisibility(RadioButton.VISIBLE);
         //exibe os dados do paciente
 		Intent intent = getIntent();
         if (intent!=null)
@@ -37,17 +50,51 @@ public class UpdatePaciente extends Activity implements OnClickListener{
         	nome.setText(paciente.nome);
         	dtnascimento.setText(paciente.datanascimento);
         }
+        //Desabilita campos. Só para exibção.
+        nome.setEnabled(false);
+        dtnascimento.setEnabled(false);
+        
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+				boolean edit = R.id.editar == checkedId;
+				boolean delete = R.id.remover == checkedId;
+				if (edit) {
+					nome.setEnabled(true);
+		            dtnascimento.setEnabled(true);
+					Log.i("jamilson", "Marcou radio Sim: " + checkedId);
+				} else if (delete) {
+					del = true;
+					nome.setEnabled(false);
+		            dtnascimento.setEnabled(false);
+					Log.i("jamilson", "Marcou radio Não: " + checkedId +""+ del);
+				}
+			}
+		});
+		
 	}
 	
 	@Override
 	public void onClick(View v) {
-		update();
+		if(del){
+			deletar();
+		}else{
+			update();
+		}
 	}
 	
 	public void update(){
 		paciente.nome = nome.getText().toString();
 		paciente.datanascimento = dtnascimento.getText().toString();
 		dao.atualizar(paciente);
+		setResult(RESULT_OK, new Intent());
+		// Fecha a tela
+		finish();
+	}
+	
+	public void deletar(){
+		paciente.nome = nome.getText().toString();
+		paciente.datanascimento = dtnascimento.getText().toString();
+		dao.deletar(paciente.id);
 		setResult(RESULT_OK, new Intent());
 		// Fecha a tela
 		finish();
@@ -62,5 +109,4 @@ public class UpdatePaciente extends Activity implements OnClickListener{
 		// Fecha a tela
 		finish();
 	}
-
 }
