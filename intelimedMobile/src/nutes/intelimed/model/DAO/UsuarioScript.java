e624ecb67b2e3f4d9da8716ab316265bec384a2e
jamilsonbatista <jamilsonbatista@gmail.com>
2011-05-24 16:56:36 +0000

    Implementação modulo de usuários

--- intelimedMobile/src/nutes/intelimed/model/DAO/UsuarioScript.java
+++ intelimedMobile/src/nutes/intelimed/model/DAO/UsuarioScript.java
@@ -10,10 +10,15 @@ public class UsuarioScript extends UsuarioDao{
 
 	// Cria a tabela com o "_id" sequencial
 	private static final String[] SCRIPT_DATABASE_CREATE = new String[] {
-			"create table usuario ( _id integer primary key autoincrement, user text not null,password text not null);",
-			"insert into usuario (user,password) values('jamilson','202cb962ac59075b964b07152d234b70');"
+			"create table permissao(_id integer primary key autoincrement, nome varchar(45), descricao varchar(45));",
+			"create table grupo (_id integer primary key autoincrement, nome varchar(45), descricao varchar(45));",
+			"create table usuario ( _id integer primary key autoincrement, user text not null,password text not null,id_grupo integer, Foreign Key (id_grupo) references grupo(_id));",
+			"create table grupo_permissao (id_grupo integer, id_permissao integer, Foreign Key (id_grupo) references grupo(_id), Foreign Key (id_permissao) references permissao(_id));",
+			"insert into permissao (nome,descricao) values('deletar','paciente');",
+			"insert into grupo (nome,descricao) values('acs','Agentes de saúde');",
+			"insert into usuario (user,password,id_grupo) values('jamilson','202cb962ac59075b964b07152d234b70', 1);"
 			 };
-
+	;
 	// Nome do banco
 	private static final String NOME_BANCO = "inteliMobile";
 
@@ -31,7 +36,6 @@ public class UsuarioScript extends UsuarioDao{
 	 */
 	
 	public UsuarioScript(Context ctx) {
-		
 		// Criar utilizando um script SQL
 		dbHelper = new SQLiteHelper(ctx, UsuarioScript.NOME_BANCO, UsuarioScript.VERSAO_BANCO,
 				UsuarioScript.SCRIPT_DATABASE_CREATE, UsuarioScript.SCRIPT_DATABASE_DELETE);
