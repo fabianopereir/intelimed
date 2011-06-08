412acff307b1f8bb6aecae1db1549150d7c3d1f9
jamilsonbatista@gmail.com <jamilsonbatista@gmail.com@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-06-08 15:10:26 +0000

    Implementation Button Back in MenuPaciente.java

--- trunk/intelimedMobile/src/nutes/intelimed/controller/activity/MenuPaciente.java
+++ trunk/intelimedMobile/src/nutes/intelimed/controller/activity/MenuPaciente.java
@@ -5,18 +5,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MenuPaciente extends Activity{
 	Button btcadPaciente, btlistPaciente, btPesquisar;
+	ImageButton back;
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menupaciente);
 		
+		back = (ImageButton) findViewById(R.bt.btBack);
 		btlistPaciente = (Button) findViewById(R.bt.opListar); 
 		btcadPaciente = (Button) findViewById(R.bt.opCadastro);
 		btPesquisar = (Button) findViewById(R.bt.opPesquisar);
@@ -40,7 +44,33 @@ public class MenuPaciente extends Activity{
 				search();
 			}
 		});
+		
+		back.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+				
+			}
+		});
 	}
+	
+	/**
+	 * @author jamilson
+	 * @Description Implementation for button  back of Activity
+	 * @param Indentification of onclick for mouse
+	 * @return value boolean
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+	        	finish();
+	        return true;
+	    }
+	    return super.onKeyDown(keyCode, event);
+	}
+	
 	/*@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
