7e620c445d8c6946b14d3560278d472353dd20a2
dyego12345@gmail.com <dyego12345@gmail.com@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-07-14 15:55:25 +0000

    Implementação dinâmica da classe DiagnosticResult.

--- trunk/BlackBox/src/nutes/intelimed/controller/activity/DiagnosticResult.java
+++ trunk/BlackBox/src/nutes/intelimed/controller/activity/DiagnosticResult.java
@@ -1,16 +1,20 @@
 package nutes.intelimed.controller.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.LinearLayout.LayoutParams;
 
 /**
  * 
@@ -20,26 +24,37 @@ import android.widget.Toast;
 public class DiagnosticResult extends Activity {
 
 	String[] questData = new String[4];
-	
 
 	public void onCreate(Bundle icicle) {
 
 		super.onCreate(icicle);
-		setContentView(R.layout.result_questionnaire);
+		//setContentView(R.layout.result_questionnaire);
 		
-		TextView resultado;
-		resultado = (TextView) findViewById(R.id.campoResultado);
-
+		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
+		layout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT));
+		layout.setBackgroundResource(R.drawable.gradientbg);
+		
+		//TextView resultado;
+		//resultado = (TextView) findViewById(R.id.campoResultado);
+		
+		/*LayoutInflater layoutInflater = (LayoutInflater) 
+        this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);    
+		layout.addView(1, layoutInflater.inflate(R.layout.navbar, this, false) ); */
+		
+		TextView resultado = new TextView(this);
+		resultado.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+		resultado.setText("testeee");
+		layout.addView(resultado);
+		
 		Intent intent = getIntent();
 		if (intent != null) {
 			questData= (String[]) intent.getSerializableExtra("questionnaireData");
 			
-			/*Toast.makeText(DiagnosticResult.this,
-					questData[0] + questData[1] + questData[2] + questData[3],
-					Toast.LENGTH_LONG).show();*/
 			System.out.println(questData[0] + questData[1] + questData[2] + questData[3]);
 			resultado.setText(questData[0] +"\n" + questData[1] +"\n"+ questData[2] +"\n"+ questData[3]);
 		}
+		setContentView(layout);
 	}
 
 }
