57852897801c1480eb4927852859c5e8f693ae57
jamilsonbatista@gmail.com <jamilsonbatista@gmail.com@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-07-19 14:39:52 +0000

    structure questionnaire correction

---
+++ trunk/BlackBox/src/nutes/intelimed/model/DAO/QuestionDao.java
@@ -0,0 +1,86 @@
+package nutes.intelimed.model.DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import nutes.intelimed.model.InterfaceModelQuestion;
+import nutes.intelimed.model.entity.Question;
+import nutes.intelimed.model.entity.Question.Perguntas;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.util.Log;
+
+public class QuestionDao implements InterfaceModelQuestion{
+	private static final String CATEGORIA = "nutes";
+	private static final String NOME_BANCO = "caixapreta";
+	public static final String NOME_TABELA = "questao";
+	
+	protected SQLiteDatabase db;
+	
+	public QuestionDao()
+	{
+		
+	}
+	public QuestionDao(Context ctx) {
+		db = ctx.openOrCreateDatabase(NOME_BANCO, Context.MODE_PRIVATE, null);
+	}
+	
+	public Cursor getCursor() {
+		try {
+			Log.i("jamilson", "dentro do metodo getCursor");
+			Cursor cursor = db.query(NOME_TABELA, Question.colunas, null, null, null,
+					null, null, null);
+			return cursor;
+		} catch (SQLException e) {
+			Log.e(CATEGORIA, "Erro ao buscar as pacientes: " + e.toString());
+			return null;
+		}
+	}
+	
+	@Override
+	public List<Question> listarPerguntas() {
+		Log.i("jamilson", "dentro do metodo listarPerguntas");
+		Cursor c = getCursor();
+		List<Question> perguntas = new ArrayList<Question>();
+		
+		int idxId = c.getColumnIndex(Perguntas.IDPERGUNTA);
+		int idxPergunta = c.getColumnIndex(Perguntas.PERGUNTA);
+		int idxMetrica = c.getColumnIndex(Perguntas.IDMETRICA);
+
+		if (c.moveToFirst()) {
+			do {
+				Question sqt = new Question();
+				perguntas.add(sqt);
+
+				sqt.id = c.getLong(idxId);
+				sqt.pergunta = c.getString(idxPergunta);
+				sqt.idmetrica = c.getInt(idxMetrica);
+				
+			} while (c.moveToNext());
+		}
+		return perguntas;
+	}
+	
+	/**
+	 * Busca um paciente utilizando as configurações definidas no
+	 * SQLiteQueryBuilder Utilizado pelo Content Provider de paciente
+	 */
+	public Cursor query(SQLiteQueryBuilder queryBuilder, String[] projection,
+			String selection, String[] selectionArgs, String groupBy,
+			String having, String orderBy) {
+		Cursor c = queryBuilder.query(this.db, projection, selection,
+				selectionArgs, groupBy, having, orderBy);
+		return c;
+	}
+	@Override
+	public void fechar() {
+		if (db != null) {
+			db.close();
+		}
+	}
+	
+}
--- trunk/BlackBox/src/nutes/intelimed/model/entity/StructureQuestionnaire.java
+++ trunk/BlackBox/src/nutes/intelimed/model/entity/StructureQuestionnaire.java
@@ -4,43 +4,60 @@ import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-
-
 public class StructureQuestionnaire {
+	public long idestrutura_questionario;
+	public int ordem;
+	public int fk_iddiagnostico;
+	public int fk_idquestao;
 	public  String pergunta;
 	public int idmetrica;
-	public long id;
 	
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
+	
 	public static final String AUTHORITY = "nutes.intelimed.model.entity";
-	public static String[] colunas = new String[] { Perguntas.IDPERGUNTA, Perguntas.PERGUNTA, Perguntas.IDMETRICA };
+	public static String[] colunas = new String[] { StructureQuestionnaireQuestion.IDESTRUTURA_QUESTIONARIO, StructureQuestionnaireQuestion.ORDDEM,
+													StructureQuestionnaireQuestion.FK_IDDIAGNOSTICO, StructureQuestionnaireQuestion.FK_IDPERGUNTA,
+													StructureQuestionnaireQuestion.PERGUNTA, StructureQuestionnaireQuestion.FK_IDMETRICA};
 	
-	public int getIdmetrica() {
-		return idmetrica;
+	public long getIdestrutura_questionario() {
+		return idestrutura_questionario;
 	}
-
-	public void setIdmetrica(int idmetrica) {
-		this.idmetrica = idmetrica;
+	public void setIdestrutura_questionario(long idestrutura_questionario) {
+		this.idestrutura_questionario = idestrutura_questionario;
 	}
-
+	public int getOrdem() {
+		return ordem;
+	}
+	public void setOrdem(int ordem) {
+		this.ordem = ordem;
+	}
+	public int getFk_iddiagnostico() {
+		return fk_iddiagnostico;
+	}
+	public void setFk_iddiagnostico(int fk_iddiagnostico) {
+		this.fk_iddiagnostico = fk_iddiagnostico;
+	}
+	public int getFk_idquestao() {
+		return fk_idquestao;
+	}
+	public void setFk_idquestao(int fk_idquestao) {
+		this.fk_idquestao = fk_idquestao;
+	}
 	public String getPergunta() {
 		return pergunta;
 	}
-
 	public void setPergunta(String pergunta) {
 		this.pergunta = pergunta;
 	}
+	public int getIdmetrica() {
+		return idmetrica;
+	}
+	public void setIdmetrica(int idmetrica) {
+		this.idmetrica = idmetrica;
+	}
 	
-	public static final class Perguntas implements BaseColumns {
+	public static final class StructureQuestionnaireQuestion implements BaseColumns {
 		
-		private Perguntas() {
+		private StructureQuestionnaireQuestion() {
 		}
 		
 		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/perguntas");
@@ -49,20 +66,23 @@ public class StructureQuestionnaire {
 	
 		public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.google.perguntas";
 	
-		public static final String DEFAULT_SORT_ORDER = "idquestao ASC";
+		public static final String DEFAULT_SORT_ORDER = "idestrutura_questionario ASC";
 	
-		public static final String IDPERGUNTA = "idquestao";	
+		public static final String IDESTRUTURA_QUESTIONARIO = "idestrutura_questionario";	
+		public static final String ORDDEM = "ordem";
+		public static final String FK_IDDIAGNOSTICO = "fk_iddiagnostico";
+		public static final String FK_IDPERGUNTA = "fk_idquestao";
 		public static final String PERGUNTA = "pergunta";
-		public static final String IDMETRICA = "fk_idmetrica";
-		
+		public static final String FK_IDMETRICA = "idmetrica";
 	
 		/**
-		 * Método que constrói uma Uri para um Paciente específico, com o seu id
+		 * Método que constrói uma Uri para um StructureQuestionnaireQuestion específico, com o seu id
 		 */
 		public static Uri getUriId(long id) {
-			Uri uriPaciente = ContentUris.withAppendedId(Perguntas.CONTENT_URI, id);
+			Uri uriPaciente = ContentUris.withAppendedId(StructureQuestionnaireQuestion.CONTENT_URI, id);
 			return uriPaciente;
 		}
 	}
-	
+
+
 }
