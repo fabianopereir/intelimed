dc6ceae39164554e49356cd37fad12bf7609f52b
jamilsonbatista@gmail.com <jamilsonbatista@gmail.com@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-07-25 15:50:14 +0000

    Search Implementation  in Structure Questionnaire. Created methods in backbox for mounting questionnaire.

--- trunk/BlackBox/src/nutes/intelimed/controller/deusdara/BlackBox.java
+++ trunk/BlackBox/src/nutes/intelimed/controller/deusdara/BlackBox.java
@@ -1,7 +1,19 @@
 package nutes.intelimed.controller.deusdara;
 
+import java.util.ArrayList;
+
+import nutes.intelimed.controller.activity.DiagnosticForm;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 /**
@@ -45,6 +57,32 @@ public class BlackBox {
 		System.out.println(res);
 		
 		return res;
+	}
+	
+	public TextView createTypeMetrics(String pergunta, String tipo, ArrayList<String> questionOption, DiagnosticForm diagnosticForm)
+	{
+		TextView perguntas = null;
+		perguntas = new TextView(diagnosticForm);
+    	perguntas.setText(pergunta);
+    	
+    	return perguntas;
+	}
+
+	public View createTypeMetricsG(String question, String tipo,ArrayList<String> questionOption, DiagnosticForm diagnosticForm) {
+		RadioGroup radio_group = new RadioGroup ( diagnosticForm );
+        RadioButton radio_button_1 = new RadioButton ( diagnosticForm );
+        radio_button_1.setId ( 1 );
+        radio_button_1.setText("Sim");
+        RadioButton radio_button_2 = new RadioButton ( diagnosticForm );
+        radio_button_2.setId ( 2 );
+        radio_button_2.setText("Não");
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams ( 200, 100 );
+        radio_group.addView ( radio_button_1, 0, params );
+        radio_group.addView ( radio_button_2, 1, params );
+        radio_group.check ( 2 );
+        radio_group.setOnCheckedChangeListener ( diagnosticForm );
+        
+		return radio_group;
 	}
 	
 }
--- trunk/BlackBox/src/nutes/intelimed/model/StructureQuestionnaireScript.java
+++ trunk/BlackBox/src/nutes/intelimed/model/StructureQuestionnaireScript.java
@@ -19,23 +19,30 @@ public class StructureQuestionnaireScript extends StructureQuestionnaireDaoTest{
 	
 	private static final String[] SCRIPT_DATABASE_CREATE = new String[] {
 			"create table padrao_validacao(idpadrao_validacao integer primary key autoincrement, padrao int, descricao varchar(255));",
-			"create table metrica (idmetrica integer primary key autoincrement, descricao varchar(20), unidade varchar(45));",
+			"create table metrica (idmetrica integer primary key autoincrement, tipo varchar(20), unidade varchar(45));",
 			"create table validacao_metrica (idvalidacao_metrica integer primary key autoincrement,fk_idmetrica integer, fk_idpadrao_validacao integer, Foreign Key (fk_idpadrao_validacao) references padrao_validacao(idpadrao_validacao),Foreign Key (fk_idmetrica) references metrica(idmetrica));",
 			"create table questao (idquestao integer primary key autoincrement, pergunta varchar(255), fk_idmetrica integer, Foreign Key (fk_idmetrica) references metrica(idmetrica));",
 			"create table diagnostico (iddiagnostico integer primary key autoincrement, date varchar(10), resultado_sis boolean, resultado_medico boolean, explicacao varchar (255));",
-			"create table estrutura_questionario (idestrutura_questionario integer, ordem integer, fk_iddiagnostico integer,fk_idquestao integer, Foreign Key (fk_iddiagnostico) references diagnostico(iddiagnostico), Foreign Key (fk_idquestao) references questao(idquestao));",
+			"create table estrutura_questionario (idestrutura_questionario integer primary key autoincrement, ordem integer, fk_iddiagnostico integer,fk_idquestao integer, Foreign Key (fk_iddiagnostico) references diagnostico(iddiagnostico), Foreign Key (fk_idquestao) references questao(idquestao));",
 			"create table resposta (idresposta integer, valor varchar(255), fk_idestrutura_questionario integer, Foreign Key (fk_idestrutura_questionario) references estrutura_questionario(idestrutura_questionario));",
 			"insert into padrao_validacao (padrao,descricao) values(1,'não');",
 			"insert into padrao_validacao (padrao,descricao) values(2,'sim');",
-			"insert into metrica (descricao,unidade) values('radio group','pergunta 1');",
+			"insert into padrao_validacao (padrao,descricao) values(3,'não sei');",
+			"insert into padrao_validacao (padrao,descricao) values(4,'não sei');",
+			"insert into metrica (tipo,unidade) values('radio group','pergunta 1');",
+			"insert into metrica (tipo,unidade) values('texto','pergunta 2');",
 			"insert into validacao_metrica (fk_idmetrica,fk_idpadrao_validacao) values(1,1);",
 			"insert into validacao_metrica (fk_idmetrica,fk_idpadrao_validacao) values(1,2);",
+			"insert into validacao_metrica (fk_idmetrica,fk_idpadrao_validacao) values(1,3);",
+			"insert into validacao_metrica (fk_idmetrica,fk_idpadrao_validacao) values(2,4);",
 			"insert into questao (pergunta,fk_idmetrica) values('Qual seu nome?',1);",
+			"insert into questao (pergunta,fk_idmetrica) values('Qual seu sobrenome?',2);",
 			"insert into diagnostico (date,resultado_sis,resultado_medico,explicacao) values('21/05/2011',1,1,'-');",
-			"insert into estrutura_questionario (ordem,fk_iddiagnostico,fk_idquestao) values(1,1,1);"
+			"insert into estrutura_questionario (ordem,fk_iddiagnostico,fk_idquestao) values(1,1,1);",
+			"insert into estrutura_questionario (ordem,fk_iddiagnostico,fk_idquestao) values(1,1,2);"
 			 };
 	private static final String NOME_BANCO = "caixapreta";
-	private static final int VERSAO_BANCO = 1;
+	private static final int VERSAO_BANCO = 2;
 	
 	//public static final String NOME_TABELA = "questao";
 	
