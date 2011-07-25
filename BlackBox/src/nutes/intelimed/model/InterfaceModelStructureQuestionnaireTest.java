c46d31ac41213ebf29227dce98fdc02270e50af4
jamilsonbatista <jamilsonbatista@gmail.com>
2011-07-25 04:33:56 +0000

    implementation of change in questionnaire structure

---
+++ BlackBox/src/nutes/intelimed/model/InterfaceModelStructureQuestionnaireTest.java
@@ -0,0 +1,10 @@
+package nutes.intelimed.model;
+
+import java.util.List;
+
+import nutes.intelimed.model.entity.StructureQuestionnaireTest;
+
+public interface InterfaceModelStructureQuestionnaireTest {
+	public abstract List<StructureQuestionnaireTest> listarEstruturaQuestionario();
+	public abstract void fechar();
+}
