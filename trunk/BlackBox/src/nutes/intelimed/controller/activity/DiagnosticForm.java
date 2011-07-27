beebf581a778a63714cca59dd7453c91a52f4a01
jamilsonbatista@gmail.com <jamilsonbatista@gmail.com@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-07-27 14:58:26 +0000

    partial implementation diagnosticResult

--- trunk/BlackBox/src/nutes/intelimed/controller/activity/DiagnosticForm.java
+++ trunk/BlackBox/src/nutes/intelimed/controller/activity/DiagnosticForm.java
@@ -16,6 +16,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -44,12 +45,11 @@ public class DiagnosticForm extends Activity implements OnCheckedChangeListener 
 	
 	ArrayList<RadioGroup> arrQuestions = new ArrayList<RadioGroup>();
 	Button validar;
-	String[] arrQuest = new String[4];
+	String[] arrQuest = new String[50];
 	JSONArray arrayJason;
 	ImageButton back, logout;
 	BlackBox treeQ;
 	JSONObject treeObj;
-	int i;
 	LinearLayout linerLayout;
 	
 	@Override
@@ -60,32 +60,7 @@ public class DiagnosticForm extends Activity implements OnCheckedChangeListener 
 		dao = (InterfaceModelStructureQuestionnaireTest) new StructureQuestionnaireScript(this);
 		montarQuest();
 		
-        /*RadioGroup radio_group = new RadioGroup ( this );
-        RadioButton radio_button_1 = new RadioButton ( this );
-        radio_button_1.setId ( 1 );
-        RadioButton radio_button_2 = new RadioButton ( this );
-        radio_button_2.setId ( 2 );
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams ( 100, 30 );
-        radio_group.addView ( radio_button_1, 0, params );
-        radio_group.addView ( radio_button_2, 1, params );
-        radio_group.check ( 2 );
-        radio_group.setOnCheckedChangeListener ( this );
-        setContentView ( radio_group );*/
-	        
-		/*rQuest1 = (RadioGroup) findViewById(R.id.rq1);
-		rQuest2 = (RadioGroup) findViewById(R.id.rq2);
-		rQuest3 = (RadioGroup) findViewById(R.id.rq3);
-		rQuest4 = (RadioGroup) findViewById(R.id.rq4);
-
-		arrQuestions.add(rQuest1);
-		arrQuestions.add(rQuest2);
-		arrQuestions.add(rQuest3);
-		arrQuestions.add(rQuest4);
-
 		validar = (Button) findViewById(R.id.ok);
-		
-		respostaQuestao();
-		
 		validar.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
@@ -106,50 +81,35 @@ public class DiagnosticForm extends Activity implements OnCheckedChangeListener 
 				System.out.println(treeObj);
 
 				Intent it = new Intent(getBaseContext(), DiagnosticResult.class);
-				it.putExtra("questionnaireData",
-						treeQ.controlTree(arrQuest, arrayJason, treeObj));
+				it.putExtra("questionnaireData",treeQ.controlTree(arrQuest, arrayJason, treeObj));
 				startActivity(it);
 			}
 
 		});
-		*/
 	}
-	 public void onCheckedChanged ( RadioGroup group, int checkedId ) 
+	
+	public void onCheckedChanged ( RadioGroup group, int checkedId ) 
 	    {
 			Toast.makeText(DiagnosticForm.this, Integer.toString(checkedId), Toast.LENGTH_SHORT).show();
+			
+			//RadioButton answer = (RadioButton) findViewById(checkedId);
+	    	
+	    	//captura resposta da questão i e armazena em array na posição i
+	    	//arrQuest[group.getId()] = answer.getTag().toString();
+			arrQuest[Integer.parseInt(group.getTag().toString())] = Integer.toString(checkedId);
 	    }
+	 
 	public void montarQuest()
 	{
 		ArrayList<StructureQuestionnaireTest>  arrayQuestion = (ArrayList<StructureQuestionnaireTest>) dao.listarEstruturaQuestionario();
-		TextView perguntas = null;
+		
 		treeQ = new BlackBox();
 		linerLayout = (LinearLayout) findViewById(R.id.LinearLayout02);
-		Iterator itr = arrayQuestion.iterator();
-		Iterator itrAux = arrayQuestion.iterator();
-		//long aux=0;
 		
 		ArrayList<String> questionOption = new ArrayList<String>();
-		String question = null;
-		
-		/*while(itr.hasNext()) {
-		    //Object element = itr.next();
-			StructureQuestionnaireTest element = (StructureQuestionnaireTest) itr.next();
-			StructureQuestionnaireTest elementAux = (StructureQuestionnaireTest) itrAux.next();
-			
-			if (element.getTipo().equals("radio group"))
-			{
-				linerLayout.addView(treeQ.createTypeMetrics(element.getPergunta(), this));
-				while (element.getIdestrutura_questionario() == elementAux.getIdestrutura_questionario() && itrAux.hasNext())
-				{
-						questionOption.add(elementAux.getDescricao());	
-						elementAux = (StructureQuestionnaireTest) itrAux.next();
-				}
-				linerLayout.addView(treeQ.createTypeMetricsG(questionOption, this));
-				questionOption.clear();
-				itr = itrAux;
-			}
-		}*/ 
+		 
 		int aux;
+		int aux2=0;
 		StructureQuestionnaireTest vAux;
 		 for(int i = 0; i < arrayQuestion.size(); i++)
 		 {
@@ -167,40 +127,14 @@ public class DiagnosticForm extends Activity implements OnCheckedChangeListener 
 								vAux = arrayQuestion.get(aux);
 							}	
 					}
-				 	linerLayout.addView(treeQ.createTypeMetricsG(questionOption, this));
+				 	aux2++;
+				 	linerLayout.addView(treeQ.createTypeMetricsG(questionOption, aux2, this));
 				 	questionOption.clear();
 				 	i = aux-1;
 				}
 		 }
 	}
 	
-	// itera em todas as questões e captura suas respectivas respostas
-	// armazenando-as em um array
-	public void respostaQuestao() {
-		
-		for (i = 0; i < arrQuestions.size(); i++) {	
-			//quando o RadioButton da questão i é marcado, então é chamado o seguinte método
-			arrQuestions.get(i).setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-				
-				public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-				    for(int cont=0;cont<arrQuest.length;cont++){  
-
-				    	RadioButton answer = (RadioButton) findViewById(checkedId);
-				    	
-				    	//captura resposta da questão i e armazena em array na posição i
-				    	arrQuest[cont] = answer.getTag().toString();
-						System.out.println("arrQuest["+cont+"]"+arrQuest[cont]);    
-				      }  
-				}
-				
-			});	
-			System.out.println("i"+i);
-
-		}
-	}
-	
-
-
 	@Override
 	protected void onPause() {
 		super.onPause();
