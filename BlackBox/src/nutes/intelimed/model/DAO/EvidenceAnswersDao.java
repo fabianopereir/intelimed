1321cfb769b126ac6b3ca014de900f59caf4b13b
jamilsonbatista <jamilsonbatista@gmail.com>
2011-08-13 04:01:49 +0000

    Implementação da classe GenericDao

--- BlackBox/src/nutes/intelimed/model/DAO/EvidenceAnswersDao.java
+++ BlackBox/src/nutes/intelimed/model/DAO/EvidenceAnswersDao.java
@@ -7,26 +7,15 @@ import nutes.intelimed.model.entity.EvidenceAnswers;
 import nutes.intelimed.model.entity.EvidenceAnswers.EvidenceAnswersTable;
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteQueryBuilder;
-import android.util.Log;
-
 
 /**
  * Classe responsável por realizar consultas em banco na tabela de evidências
  * @author Jamilson Batista (jamilsonbatista@gmail.com)
  * @author Dyego Carlos (dyego12345@gmail.com)
  */
-public class EvidenceAnswersDao implements IModelEvidenceAnswers {
-	private static final String CATEGORIA = "nutes";
-	private static final String NOME_BANCO = "caixapreta";
-
+public class EvidenceAnswersDao extends GenericDao implements IModelEvidenceAnswers {
+	
 	public static final String NOME_TABELA = "evidencia_respostas";
-
-	protected SQLiteDatabase db;
 
 	public EvidenceAnswersDao() {}
 
@@ -50,41 +39,6 @@ public class EvidenceAnswersDao implements IModelEvidenceAnswers {
 		return id;
 	}
 	
-	/**
-	  * Método responsável pela captura do cursor 
-	  * @return Cursor - cursor para consulta ao banco de dados
-	  */
-	public Cursor getCursor() {
-		try {
-			Cursor cursor = db.query(NOME_TABELA, EvidenceAnswers.colunas, null, null,
-					null, null, null, null);
-			return cursor;
-		} catch (SQLException e) {
-			Log.e(CATEGORIA, "Erro ao buscar: " + e.toString());
-			return null;
-		}
-	}
-	
-	/**
-	 * Busca utilizando as configurações definidas no SQLiteQueryBuilder
-	 *    Utilizado pelo Content Provider de respostas de evidência
-	 * @param queryBuilder
-	 * @param projection - condição de projeção
-	 * @param selection - condição de seleção
-	 * @param selectionArgs - argumentos da seleção
-	 * @param groupBy - condição de agrupamento
-	 * @param having - condição
-	 * @param orderBy - condição de ordenamento
-	 * @return Cursor - cursor com o retorno da consulta desejada
-	 */
-	public Cursor query(SQLiteQueryBuilder queryBuilder, String[] projection,
-			String selection, String[] selectionArgs, String groupBy,
-			String having, String orderBy) {
-		Cursor c = queryBuilder.query(this.db, projection, selection,
-				selectionArgs, groupBy, having, orderBy);
-		return c;
-	}
-
 	@Override
 	public void fechar() {
 		if (db != null) {
