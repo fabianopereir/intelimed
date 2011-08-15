74518fe8b6bd609997037de98c347a08cbb43a7c
jamilsonbatista <jamilsonbatista@gmail.com>
2011-08-15 21:45:33 +0000

    Acrescentanto comentário em IBlackBox

--- BlackBox/src/nutes/intelimed/controller/IBlackBox.java
+++ BlackBox/src/nutes/intelimed/controller/IBlackBox.java
@@ -3,9 +3,14 @@ package nutes.intelimed.controller;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+/**
+ * Interface de BlackBox
+ * @author Jamilson Batista (jamilsonbatista@gmail.com)
+ * @author Dyego Carlos (dyego12345@gmail.com)	
+ * 
+ */
 public interface IBlackBox {
 
-	public abstract String controlTree(String[] arrQuest, JSONArray arrayJason,
-			JSONObject treeObj, String[] arrNO);
+	public abstract String controlTree(String[] arrQuest, JSONArray arrayJason,JSONObject treeObj, String[] arrNO);
 
 }
