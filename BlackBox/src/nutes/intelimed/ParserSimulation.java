fd7d7851f8d763f1c57ad0bfff5c70f941237a8e
jamilsonbatista <jamilsonbatista@gmail.com>
2011-08-19 16:01:43 +0000

    Iniciando implementação do "Parser" do Json

--- BlackBox/src/nutes/intelimed/ParserSimulation.java
+++ BlackBox/src/nutes/intelimed/ParserSimulation.java
@@ -4,24 +4,44 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.R.array;
+
 
 public class ParserSimulation {
-
+	private JSONObject jObject;
+	
+	//private String jString = "{\"menu\":	{\"id\": \"file\", \"value\": \"File\", \"popup\": { \"menuitem\": [ {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"}, {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"}, {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}]}}}";
+	private String jString = "{'menu':	{'id': 'file', 'value': 'File', 'popup': { 'menuitem': [ {'value': 'New', 'onclick': 'CreateNewDoc()'}, {'value': 'Open', 'onclick': 'OpenDoc()'}, {'value': 'Close', 'onclick': 'CloseDoc()'}]}}}";
 	public ParserSimulation()
 	{
 		
 	}
 	public void parserJson(String arquivo)
 	{
-		JSONObject teste = new JSONObject();
+		//JSONObject teste = new JSONObject();
 		try {
 			
-			
-			//System.out.println("Propriedade1"+teste.get(arquivo));
-			System.out.println("Propriedade1"+teste.getJSONObject(arquivo));
+			jObject = new JSONObject(jString);
+
+			JSONObject menuObject = jObject.getJSONObject("menu");
+			String attributeId = menuObject.getString("id");
+			System.out.println(attributeId);
+
+			String attributeValue = menuObject.getString("value");
+			System.out.println(attributeValue);
+
+			JSONObject popupObject = menuObject.getJSONObject("popup");
+			JSONArray menuitemArray = popupObject.getJSONArray("menuitem");
+
+			for (int i = 0; i < 3; i++) {
+				System.out.println(menuitemArray.getJSONObject(i).getString("value").toString());
+				System.out.println(menuitemArray.getJSONObject(i).getString("onclick").toString());
+			}
+
 			
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 	}
