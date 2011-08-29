b964f72533ba5dd584575aafe113af86ab594e23
rodrigo.aosilva <rodrigo.aosilva@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-08-29 23:26:36 +0000

    Alteração das URL's do banco.

--- Intermediate/grails-app/conf/DataSource.groovy
+++ Intermediate/grails-app/conf/DataSource.groovy
@@ -1,6 +1,8 @@
 dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
+	//driverClassName = "org.hsqldb.jdbcDriver"
+	//username = "sa"
     username = "root"
     password = ""
 }
@@ -14,19 +16,21 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:mysql://localhost:3306/intermediate"
+            url = "jdbc:mysql://localhost:3306/intermediate_dev"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
+            //url = "jdbc:hsqldb:mem:testDb"
+			url = "jdbc:mysql://localhost:3306/intermediate_test"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+            //url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+			url = "jdbc:mysql://localhost:3306/intermediate"
         }
     }
 }
