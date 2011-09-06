7eb05a2ff493c69fb08b1c82ba5ca736a0cf564b
dyego12345@gmail.com <dyego12345@gmail.com@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-09-06 15:46:31 +0000

    Início de implementação do parser para leitura de JSON

--- trunk/BlackBox/src/nutes/intelimed/ParserSimulation.java
+++ trunk/BlackBox/src/nutes/intelimed/ParserSimulation.java
@@ -14,8 +14,8 @@ import org.json.JSONObject;
 public class ParserSimulation {
 	private JSONObject jObject;
 	
-	private String jString = "{'menu':	{'id': 'file', 'value': 'File', 'popup': { 'menuitem': [ {'value': 'New', 'onclick': 'CreateNewDoc()'}, {'value': 'Open', 'onclick': 'OpenDoc()'}, {'value': 'Close', 'onclick': 'CloseDoc()'}]}}}";
-	
+	private String jString = "[{'class':'intermediate.Arvore','id':1,'arestas':[{'class':'intermediate.Aresta','id':1,'resposta':{'class':'intermediate.Resposta','id':1,'descricao':'Sunny'}},{'class':'intermediate.Aresta','id':2,'resposta':{'class':'intermediate.Resposta','id':2,'descricao':'Overcast'}},{'class':'intermediate.Aresta','id':3,'resposta':{'class':'intermediate.Resposta','id':3,'descricao':'Rain'}},{'class':'intermediate.Aresta','id':7,'resposta':{'class':'intermediate.Resposta','id':7,'descricao':'Weak'}},{'class':'intermediate.Aresta','id':5,'resposta':{'class':'intermediate.Resposta','id':5,'descricao':'Normal'}},{'class':'intermediate.Aresta','id':4,'resposta':{'class':'intermediate.Resposta','id':4,'descricao':'High'}},{'class':'intermediate.Aresta','id':6,'resposta':{'class':'intermediate.Resposta','id':6,'descricao':'Strong'}}],'nos':[{'class':'intermediate.No','id':1,'arestasEntrada':[],'descricao':'Outlook','respostas':[{'class':'intermediate.Resposta','id':2,'descricao':'Overcast'},{'class':'intermediate.Resposta','id':1,'descricao':'Sunny'},{'class':'intermediate.Resposta','id':3,'descricao':'Rain'}]},{'class':'intermediate.No','id':2,'arestasEntrada':[{'class':'intermediate.Aresta','id':1,'resposta':{'class':'intermediate.Resposta','id':1,'descricao':'Sunny'}}],'descricao':'Humidity','respostas':[{'class':'intermediate.Resposta','id':4,'descricao':'High'},{'class':'intermediate.Resposta','id':5,'descricao':'Normal'}]},{'class':'intermediate.No','id':3,'arestasEntrada':[{'class':'intermediate.Aresta','id':3,'resposta':{'class':'intermediate.Resposta','id':3,'descricao':'Rain'}}],'descricao':'Wind','respostas':[{'class':'intermediate.Resposta','id':6,'descricao':'Strong'},{'class':'intermediate.Resposta','id':7,'descricao':'Weak'}]},{'class':'intermediate.No','id':4,'arestasEntrada':[{'class':'intermediate.Aresta','id':2,'resposta':{'class':'intermediate.Resposta','id':2,'descricao':'Overcast'}},{'class':'intermediate.Aresta','id':7,'resposta':{'class':'intermediate.Resposta','id':7,'descricao':'Weak'}},{'class':'intermediate.Aresta','id':5,'resposta':{'class':'intermediate.Resposta','id':5,'descricao':'Normal'}}],'descricao':'Yes','respostas':[]},{'class':'intermediate.No','id':5,'arestasEntrada':[{'class':'intermediate.Aresta','id':4,'resposta':{'class':'intermediate.Resposta','id':4,'descricao':'High'}},{'class':'intermediate.Aresta','id':6,'resposta:{'class':'intermediate.Resposta','id':6,'descricao':'Strong'}}],'descricao':'No','respostas':[]}],'respostas':[{'class':'intermediate.Resposta','id':2,'descricao':'Overcast'},{'class':'intermediate.Resposta','id':6,'descricao':'Strong'},{'class':'intermediate.Resposta','id':1,'descricao':'Sunny'},{'class':'intermediate.Resposta','id':4,'descricao':'High'},{'class':'intermediate.Resposta','id':7,'descricao':'Weak'},{'class':'intermediate.Resposta','id':5,'descricao':'Normal'},{'class':'intermediate.Resposta','id':3,'descricao':'Rain'}]}]";
+							
 	public ParserSimulation(){
 		
 	}
@@ -27,9 +27,31 @@ public class ParserSimulation {
 	 */
 	public void parserJson(String arquivo){
 		try {
+			//jObject = new JSONObject(arquivo);
 			
 			jObject = new JSONObject(jString);
-
+			
+			JSONObject arvoreObject = jObject.getJSONObject("intermediate.Arvore");
+			String attributeId = arvoreObject.getString("id");
+			System.out.println(attributeId);
+			
+			JSONObject arestasObject = arvoreObject.getJSONObject("arestas");
+			JSONArray arestaArray = arestasObject.getJSONArray("intermediate.Aresta");
+			for (int i = 0; i < 3; i++) {
+				String attributeIdAresta = arestaArray.getJSONObject(i).getString("id");
+				System.out.println(attributeIdAresta);
+				JSONObject respostaObject = arvoreObject.getJSONObject("resposta");
+				for(int j = 0; j < 2; j++){
+					String attributeIdResposta = respostaObject.getString("id").toString();
+					System.out.println(attributeIdResposta);
+					String attributeDescricaoResposta = respostaObject.getString("descricao");
+					System.out.println(attributeDescricaoResposta);
+				}
+			}
+			
+			/*
+			jObject = new JSONObject(jString);
+			
 			JSONObject menuObject = jObject.getJSONObject("menu");
 			String attributeId = menuObject.getString("id");
 			System.out.println(attributeId);
@@ -44,7 +66,7 @@ public class ParserSimulation {
 				System.out.println(menuitemArray.getJSONObject(i).getString("value").toString());
 				System.out.println(menuitemArray.getJSONObject(i).getString("onclick").toString());
 			}
-
+			*/
 			
 		} catch (JSONException e) {
 			
