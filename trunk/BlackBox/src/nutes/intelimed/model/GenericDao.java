ae7cf10b34886b99219c72c0e54b8a59decfbe2f
brunocarlos06 <brunocarlos06@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-10-20 21:22:30 +0000

    Versão com árvore da asma.

--- trunk/BlackBox/src/nutes/intelimed/model/GenericDao.java
+++ trunk/BlackBox/src/nutes/intelimed/model/GenericDao.java
@@ -14,7 +14,7 @@ public class GenericDao {
 	protected SQLiteDatabase db;
 	protected static final String CATEGORIA = "nutes";
 	protected static final String NOME_BANCO = "caixapreta";
-	protected static final int VERSAO_BANCO = 36;
+	protected static final int VERSAO_BANCO = 40;
 	
 	/**
 	 * Busca utilizando as configurações definidas no SQLiteQueryBuilder
