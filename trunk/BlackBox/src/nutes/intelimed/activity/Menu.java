ab26c5befddb23b6546944209e38852e72594944
dyego12345@gmail.com <dyego12345@gmail.com@8b2ae22e-fff0-188b-34d7-3072e707e3dc>
2011-11-03 15:00:18 +0000

    Refact Tela Menu

--- trunk/BlackBox/src/nutes/intelimed/activity/Menu.java
+++ trunk/BlackBox/src/nutes/intelimed/activity/Menu.java
@@ -3,6 +3,7 @@ package nutes.intelimed.activity;
 import nutes.intelimed.R;
 import nutes.intelimed.controller.evidence.EvidenceController;
 import nutes.intelimed.controller.evidence.IEvidence;
+import nutes.intelimed.controller.evidence.SendEvidence;
 import nutes.intelimed.controller.tree.ITree;
 import nutes.intelimed.controller.tree.TreeController;
 import org.json.JSONException;
@@ -11,6 +12,8 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -24,11 +27,11 @@ import android.widget.Button;
  * @author Dyego Carlos (dyego12345@gmail.com)
  * 
  */
-public class Menu extends Activity{
+public class Menu extends Activity {
 	
 	private ProgressDialog dialog;
 		
-	private Button logout, diagnostic, sync, TreeUp; 
+	private Button logout, diagnostic, sync, treeUp; 
 	
 	protected static final String CATEGORIA = "nutes";
 	
@@ -40,26 +43,23 @@ public class Menu extends Activity{
 		
         diagnostic = (Button) findViewById(R.id.btDiagnostico);
         sync = (Button) findViewById(R.id.btSincronizarDados);
-        TreeUp = (Button) findViewById(R.id.btAtualizarArvore);
+        treeUp = (Button) findViewById(R.id.btAtualizarArvore);
         logout = (Button) findViewById(R.bt.btLogoff);
         
-        //back = (Button) findViewById(R.bt.btBack);
-        //back.setVisibility(Button.INVISIBLE);
-
-		diagnostic.setOnClickListener(new OnClickListener() {
+       	diagnostic.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				Intent it = new Intent(getBaseContext(), FormDiagnostic.class);
 				startActivity(it);
 			}
 		});
 		
-		sync.setOnClickListener(new OnClickListener() {
+       	sync.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				sendData();
 			}
 		});	
 		
-		TreeUp.setOnClickListener(new OnClickListener() {
+		treeUp.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				treeUpdate();
 			}
@@ -74,33 +74,74 @@ public class Menu extends Activity{
 		
 	}
 	
+	private Handler handler = new Handler() {
+		 @Override
+		 public void handleMessage(Message msg) {     
+
+		 }
+		};
+
+		
+	
+
 	/**
 	 * Evento disparado quando o botão de "sincronizar dados" da Activity Menu recebe um click
 	 * @return void
 	 */
 	public void sendData(){
-		dialog = ProgressDialog.show(this,"InteliMED", "Enviando dados", false,true);
-		try {
-			IEvidence evidence = new EvidenceController(this);
-			evidence.sendEvidence();
-		} catch (JSONException e) {
-			e.printStackTrace();
-		} finally {
+		dialog = ProgressDialog.show(Menu.this, "InteliMED", "Sincronizando Dados...");
+
+		new Thread() {
+
+			public void run() {
+	
+				try {
+					IEvidence evidence = new EvidenceController(Menu.this);
+					evidence.sendEvidence();
+				} catch (JSONException e) {
+					e.printStackTrace();
+				} 
+	
+			// dismiss the progress dialog
+			handler.sendEmptyMessage(0);
 			dialog.dismiss();
-		}
+	
+			}
+
+		}.start();
+
+		
+		
+		
              
 	}
 	
 	private void treeUpdate(){
-		dialog = ProgressDialog.show(Menu.this,"InteliMED", "Atualizando árvore...", false,true);
-		try {
-			ITree tree = new TreeController(getBaseContext());
-			tree.receiveTree();
-		}catch (Exception e) {
-			Log.i(CATEGORIA, e.getMessage(),e);
-		}finally{
-			dialog.dismiss();	
-		}
+		dialog = ProgressDialog.show(Menu.this, "InteliMED", "Sincronizando Dados...");
+
+		new Thread() {
+
+			public void run() {
+	
+				try {
+					ITree tree = new TreeController(getBaseContext());
+					tree.receiveTree();
+				}catch (Exception e) {
+					Log.i(CATEGORIA, e.getMessage(),e);
+				}
+	
+			// dismiss the progress dialog
+			handler.sendEmptyMessage(0);
+			dialog.dismiss();
+	
+			}
+
+		}.start();
+		
+		
+		Log.i("nutes","dentro do treeUpdate");
+		
+		
 	}
 	
 	/**
